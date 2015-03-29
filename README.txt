f9ed297efd243e1c3fe9727d3fb816be02c70073
unknown <ryanchrisco@gmail.com>
2015-03-29 14:21:51 -0500

    Final version 1.0 commit.

--- README.txt
+++ README.txt
@@ -1,1 +1,37 @@
-Place your catalog project in this directory.
+AUTHOR:	
+	Ryan Chrisco, 3/29/2015
+FILES: 
+	project.py
+	database_Setup.py
+	static/style.css
+	templates/layout.html
+	templates/home.html
+	templates/category_list.html
+	templates/item.html
+	templates/add_item.html
+	templates/add_user.html
+	templates/delete.html
+	templates/edit.html
+	templates/login.html
+	templates/add_category.html
+	templates/error.html
+DESCRIPTION: 
+	A website that stores and displays a list of "items", grouped by "category", along with descriptions.
+DIRECTIONS: 
+	Install Postgres SQL 9.3.6
+    Create a database named "item_catalog.db" in Postgres by issuing the command "createdb item_catalog.db" from the shell
+    Install Python 2.7.6
+    Run "database_Setup.py" from the shell (this creates all necessary DB objects)
+    Run "project.py" from the shell (this starts the webservice)
+    Access it at "localhost:5000/home" (URL mappings are identical to those in the demonstration project)
+    	-localhost:5000/catalog/<CATEGORY_NAME>/items			Displays a list of items in a specified category
+    	-localhost:5000/catalog/<CATEGORY_NAME>/<ITEM_NAME>		Displays specific item
+    	-localhost:5000/catalog/<ITEM_NAME>/edit				Displays an item and allows for editing
+    	-localhost:5000/catalog/<ITEM_NAME>/delete				Displays an item and prompts for deletion
+    	-localhost:5000/catalog/add_item						Displays a form that allows an item to be added (must be logged in)
+    	-localhost:5000/catalog/add_category					Displays a form that allows a category to be added (must be logged in)
+    	-localhost:5000/catalog/add_user						Displays a form that allows a user to be added (must be logged in)
+    	-localhost:5000/catalog.json							Displays a JSON string response
+NOTES: 
+	The webservice creates a default user under the name password "admin/admin" for ease of testing.
+	Once logged in, any user can create another user (but may not delete old users).
--- project.py
+++ project.py
@@ -18,22 +18,25 @@ db_session.merge(BasicUser)
 db_session.commit()
 
 # AUTHORIZATION LOGIC ##################################
+#Display page for login
 @app.route('/catalog/login')
 def DisplayLogin():
 	return render_template('login.html')
 
-@app.route('/catalog/auth_user', methods=['POST'])
+#Method for determining if a user is in the system
+@app.route('/catalog/auth_user', methods=['POST']) 
 def auth_user():
 	username = request.form['username']
 	password = request.form['password']
 	resultSetCount = db_session.query(Users).filter(and_(Users.username==username,Users.password==password)).count()
-	print resultSetCount
+	#If a record matches the users credentials; they are a user
 	if resultSetCount > 0:
 		session['username'] = username
 		return redirect(url_for('DisplayHome'))
 	else:
+		#Alert if the user was not actually in the system
 		return render_template('login.html', message='No user/password combination found.')
-
+#Routing that removes the user's login from their session (logging them out)
 @app.route('/catalog/logout', methods=['POST'])
 def signout():
 	if 'username' not in session:
@@ -41,6 +44,7 @@ def signout():
 	session.pop('username',None)
 	return redirect(url_for('DisplayHome'))
 
+#Flask decorator to define which routing methods below require authorization
 def auth_user(f):
 	@wraps(f)
 	def check_user_status(*args, **kwargs):
@@ -53,39 +57,53 @@ def auth_user(f):
 @app.route('/')
 @app.route('/home')
 def DisplayHome():
-	#Displays categories and latest items
-	categories = db_session.query(Categories).all()
-	latest_items = db_session.query(Items).order_by(desc(Items.date_added)).limit(10)
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
+	#Displays all categories and the 10 most recently added items
+	try:
+		categories = db_session.query(Categories).all()
+		latest_items = db_session.query(Items).order_by(desc(Items.date_added)).limit(10)
+	except:
+		print 'An error has occurred in DisplayHome()'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return render_template('home.html', categories=categories, items=latest_items, loggedIn=loggedIn)
 
 @app.route('/catalog/<string:category_id>/<string:category_item_id>/')
 def DisplayCatalogItem(category_id, category_item_id):
-	print category_item_id 
-	item = db_session.query(Items).filter_by(name=category_item_id).first()
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
+	#Grab the item from the item table
+	try:
+		item = db_session.query(Items).filter_by(name=category_item_id).first()
+	except:
+		print 'An error has occured in DisplayCatalogItem.'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return render_template('item.html', Item=item, loggedIn=loggedIn)
 
 @app.route('/catalog/<string:category_id>/items')
 def DisplayCategory(category_id):
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
 	try:
+		#Grab all categories and then the list of items in the specified category
 		categories = db_session.query(Categories).all()
 		items_in_category = db_session.query(Items).filter_by(category=category_id).all()
 	except:
 		print 'Error in DisplayCategory()'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return render_template('category_list.html', items=items_in_category, categories=categories, loggedIn=loggedIn)
 
 
 @app.route('/catalog/add_user')
 @auth_user
-def DisplayAddUser():	
+def DisplayAddUser():
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
@@ -94,56 +112,96 @@ def DisplayAddUser():
 @app.route('/catalog/<string:category_item_id>/edit')
 @auth_user
 def DisplayEditItem(category_item_id):
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
-	categories = db_session.query(Categories).all()
-	item = db_session.query(Items).filter_by(name=category_item_id).first()
+	try:
+		categories = db_session.query(Categories).all()
+		item = db_session.query(Items).filter_by(name=category_item_id).first()
+	except:
+		print 'Error in DisplayEditItem'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return render_template('edit.html', OldItem=item, options=categories, loggedIn=loggedIn)
 
 @app.route('/catalog/<string:category_item_id>/delete')
 @auth_user
 def DisplayDeleteItem(category_item_id):
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
-	item = db_session.query(Items).filter_by(name=category_item_id).first()
-	return render_template('delete.html', item=item, loggedIn=loggedIn)
+	try:
+		item = db_session.query(Items).filter_by(name=category_item_id).first()
+	except:
+		print 'An error has occurred in DisplayDeleteItem()'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
+	return render_template('delete.html', item=item, loggedIn=loggedIn, message='Are you sure you want to delete this item?')
 
 @app.route('/catalog/add_item')
 @auth_user
 def DisplayAddItem():
+	#Check login status and send it to the template
 	loggedIn = False
 	if 'username' in session:
 		loggedIn = True
 	try:
+		#Grab all categories to display in page
 		options = db_session.query(Categories).all()
 	except:
-		print 'Error in DisplayAddItem().'	
+		print 'Error in DisplayAddItem().'
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return render_template('add_item.html', options=options, loggedIn=loggedIn)
+
+@app.route('/catalog/add_category')
+@auth_user
+def DisplayAddCategory():
+	#Check login status and send it to the template
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
+	return render_template('add_category.html', loggedIn=loggedIn)
 
 @app.route('/catalog.json')
 def DisplayCatalogJson():
-	full_catalog = db_session.query(Items).all()
+	try:
+		#Grab all of the items from the DB
+		full_catalog = db_session.query(Items).all()
+	except:
+		print 'An error has occcurred in DisplayCatalogJson()'
+		return "{message : 'ERROR'}"
+	#Return the JSON-strong of the list of items under the array name "AllItems"
 	return jsonify(AllItems=[i.serialize for i in full_catalog])
 
 # LOGIC CONTROLLERS #########################################
+# All of the logic controllers intercept on POST to receive form submissions
 @app.route('/catalog/delete_item', methods=['POST'])
 def delete_post():
+	#Check login status and send it to the template if necessary
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
 	try:
+		#Grab the desired deletion target (by name), then delete and commit
 		itemName = request.form['TargetName']
 		targetItem = db_session.query(Items).filter_by(name=itemName).first()
 		db_session.delete(targetItem);
 		db_session.commit()
 	except:
 		print 'Error in delete_post(). Rolling back.'
-		db_session.rollback()
+		db_session.rollback() #Be sure to rollback due to the exception
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return redirect(url_for('DisplayHome'))
 		
 
 @app.route('/catalog/edit_item', methods=['POST'])
 def edit_post():
+	#Check login status and send it to the template if necessary
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
 	try:
+		#Grab the new item details from the form, create an item in the ORM, then merge it into the table
 		itemName = request.form['NewName']
 		itemDescription = request.form['NewDescription']
 		itemCategory = request.form['NewCategory']
@@ -152,13 +210,19 @@ def edit_post():
 		db_session.commit()
 	except:
 		print 'Error in edit_post(). Rolling back.'
-		db_session.rollback()
+		db_session.rollback() #Be sure to rollback in the case of an exception
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return redirect(url_for('DisplayHome'))
 		
 
 @app.route('/catalog/add_item', methods=['POST'])
 def add_post():
+	#Check login status and send it to the template if necessary
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
 	try:
+		#Grab the new item and add/commit it to the DB
 		itemName = request.form['ItemName']
 		itemDescription = request.form['ItemDescription']
 		itemCategory = request.form['ItemCategory']
@@ -167,24 +231,51 @@ def add_post():
 		db_session.commit()
 	except:
 		print 'Error in add_post(). Rolling back.'
-		db_session.rollback()
+		db_session.rollback() #Rollback DB in case of error
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return redirect(url_for('DisplayHome'))
 
 @app.route('/catalog/add_user', methods=['POST'])
 def add_user():
+	#Check login status and send it to the template if necessary
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
 	try:
+		#Grab the new user data from the form and add/commit it to the DB
 		realName = request.form['RealName']
 		userName = request.form['Username']
 		password = request.form['Password']
 		cpass = request.form['ConfirmPassword']
+		#If the passwords don't match, notify the user and do not insert
 		if cpass != password:
-			return render_template('add_user.html', message='Passwords do not match. Try again.')
+			return render_template('add_user.html', message='Passwords do not match. Try again.', loggedIn=loggedIn)
 		newUser = Users(name=realName, username=userName, password=password)
 		db_session.add(newUser)
 		db_session.commit()
 	except:
 		print 'Error in add_user(). Rolling back.'
-		db_session.rollback()
+		db_session.rollback() #Be sure to rollback if there's an exception
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
+	return redirect(url_for('DisplayHome'))
+
+@app.route('/catalog/add_category', methods=['POST'])
+def add_category():
+	#Check login status and send it to the template if necessary
+	loggedIn = False
+	if 'username' in session:
+		loggedIn = True
+	try:
+		#Grab new category information from the form and add/commit it to the DB
+		catName = request.form['CategoryName']
+		catDescription = request.form['CategoryDescription']
+		newCategory = Categories(name=catName, description=catDescription, date_added=func.now())
+		db_session.add(newCategory)
+		db_session.commit()
+	except:
+		print 'Error in add_category(). Rolling back.'
+		db_session.rollback() #Be sure to rollback on an exception
+		return render_template('error.html', loggedIn=loggedIn, message='An unrecoverable error has occured.')
 	return redirect(url_for('DisplayHome'))
 
 # DEBUG CODE #############################################
